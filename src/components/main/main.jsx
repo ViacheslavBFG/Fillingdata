e6f4e3f2ee12fc05f14371c1ed967dbdbd9bbbc1
ViacheslavBFG <131298066+ViacheslavBFG@users.noreply.github.com>
2024-02-18 22:47:14 +0200

    add validation

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { MainDiv, NameDiv, EmailDiv, BtnDiv, BtnStyle } from './mainStyle';
 import { Pthird } from 'components/thirdstep/thirdstepStyle';
 import { TextField } from '@mui/material';
 
-const main = () => {
+const Main = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [formValid, setFormValid] = useState(false);
+
+  const handleInputChange = e => {
+    const { name, value } = e.target;
+
+    if (name === 'firstName') {
+      setFirstName(value);
+    } else if (name === 'lastName') {
+      setLastName(value);
+    } else if (name === 'email') {
+      setEmail(value);
+    }
+  };
+
+  const validateForm = () => {
+    return (
+      firstName.trim() !== '' && lastName.trim() !== '' && email.trim() !== ''
+    );
+  };
+
+  const handleFormValidation = () => {
+    setFormValid(validateForm());
+  };
+
+  const handleInputBlur = () => {
+    handleFormValidation();
+  };
+
   return (
     <MainDiv>
       <h2>Calculation Form</h2>
@@ -15,32 +46,47 @@ const main = () => {
       <NameDiv>
         <TextField
           id="outlined-basic"
+          name="firstName"
           label="First name"
           variant="outlined"
           size="small"
+          value={firstName}
+          onChange={handleInputChange}
+          onBlur={handleInputBlur}
+          fullWidth
         />
         <TextField
           id="outlined-basic"
+          name="lastName"
           label="Last name"
           variant="outlined"
           size="small"
+          value={lastName}
+          onChange={handleInputChange}
+          onBlur={handleInputBlur}
+          fullWidth
         />
       </NameDiv>
       <EmailDiv>
         <TextField
           id="outlined-basic"
+          name="email"
           label="Email"
           variant="outlined"
           size="small"
+          value={email}
+          onChange={handleInputChange}
+          onBlur={handleInputBlur}
+          fullWidth
         />
       </EmailDiv>
       <BtnDiv>
-        <Link to={'/secondstep'}>
-          <BtnStyle>NEXT</BtnStyle>
+        <Link to={formValid ? '/secondstep' : '#'}>
+          <BtnStyle disabled={!formValid}>NEXT</BtnStyle>
         </Link>
       </BtnDiv>
     </MainDiv>
   );
 };
 
-export default main;
+export default Main;
